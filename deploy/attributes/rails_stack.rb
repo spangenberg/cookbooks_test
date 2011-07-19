2ac2d5962d7712094bd5c7c39dd4e24d9122ec00
Daniel Spangenberg <daniel.spangenberg@parcydo.com>
2011-07-19 17:25:01 +0200

    #4

--- deploy/attributes/rails_stack.rb
+++ deploy/attributes/rails_stack.rb
@@ -1,13 +1,17 @@
 default[:scalarium][:rails_stack][:name] = "apache_passenger"
 case default[:scalarium][:rails_stack][:name]
 when "apache_passenger"
+  Chef::Log.error("Setting to passenger")
   default[:scalarium][:rails_stack][:recipe] = "passenger_apache2::rails"
   default[:scalarium][:rails_stack][:needs_reload] = true
   default[:scalarium][:rails_stack][:service] = 'apache2'
 when "nginx_unicorn"
+  Chef::Log.error("Setting to unicorn")
   default[:scalarium][:rails_stack][:recipe] = "unicorn::rails"
   default[:scalarium][:rails_stack][:needs_reload] = true
   default[:scalarium][:rails_stack][:service] = 'unicorn'
 else
   raise "Unknown stack: #{default[:scalarium][:rails_stack][:name].inspect}"
 end
+
+Chef::Log.error("Set to: #{default[:scalarium][:rails_stack][:recipe].inspect}")
