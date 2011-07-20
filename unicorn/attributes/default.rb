d44324d7f696bebc313e43289e41208acf33997e
Daniel Spangenberg <daniel.spangenberg@parcydo.com>
2011-07-20 16:19:55 +0200

    foo

--- unicorn/attributes/default.rb
+++ unicorn/attributes/default.rb
@@ -1,4 +1,4 @@
 default[:unicorn][:worker_processes] = node[:rails][:max_pool_size] ? node[:rails][:max_pool_size] : 4
-default[:unicorn][:worker_processes] = 60
+default[:unicorn][:timeout] = 60
 default[:unicorn][:backlog] = 64
 default[:unicorn][:preload_app] = false
