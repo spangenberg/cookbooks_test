47b22520f5ce7dfdd923b3dbdf046d727751442e
Daniel Spangenberg <daniel.spangenberg@parcydo.com>
2011-07-20 12:18:07 +0200

    foo

--- deploy/definitions/scalarium_deploy.rb
+++ deploy/definitions/scalarium_deploy.rb
@@ -110,6 +110,7 @@ define :scalarium_deploy do
   # Need to be uncommented in production
   # XXX
   if deploy[:application_type] == 'rails'# && node[:scalarium][:instance][:roles].include?('rails-app')
+    Chef::Log.error(node[:scalarium][:rails_stack].inspect)
     case node[:scalarium][:rails_stack]
 
     when 'apache_passenger'
