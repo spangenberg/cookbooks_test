0a6ced47df689260b95d8dcc9feef4397d2767d8
Daniel Spangenberg <daniel.spangenberg@parcydo.com>
2011-07-21 15:45:22 +0200

    Foo

--- deploy/recipes/rails-undeploy.rb
+++ deploy/recipes/rails-undeploy.rb
@@ -35,8 +35,6 @@ node[:deploy].each do |application, deploy|
     notifies :restart, resources(:service => node[:scalarium][:rails_stack][:service])
 
   when 'nginx_unicorn'
-    include_recipe "unicorn::service"
-
     link "/etc/nginx/sites-enabled/#{application}" do
       action :delete
       only_if do 
