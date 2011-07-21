3dfc8bb49f388da5bddf9927de6b163235e5ccd0
Daniel Spangenberg <daniel.spangenberg@parcydo.com>
2011-07-21 17:49:22 +0200

    Foo

--- nginx/definitions/nginx_site.rb
+++ nginx/definitions/nginx_site.rb
@@ -1,1 +1,11 @@
-# ALIAS
+define :nginx_web_app, :template => "site.erb", :enable => true do
+  application = params[:application]
+  application_name = params[:name]
+
+  Chef::Log.debug("The \"nginx_site\" will be deprecated, please use nginx_web_app instead.")
+
+  nginx_web_app do
+    application application
+    application_name application_name
+  end
+end
