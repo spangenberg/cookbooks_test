d3fbf578cfee49a97ca5d098ddd317d6a2b18c88
Daniel Spangenberg <daniel.spangenberg@parcydo.com>
2011-07-22 16:27:20 +0200

    more gem support lib to dependencies and get rid of gem_support cookbook

--- gem_support/metadata.rb
+++
@@ -1,4 +0,0 @@
-maintainer        "Peritor GmbH"
-maintainer_email  "scalarium@peritor.com"
-description       "Offers a few library functions to simplify dealing with RubyGems"
-version           "0.1"
